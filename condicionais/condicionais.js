7b1967fe13f22fc4a2fa9b338c71ffc898d705bc
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-07 17:19:10 -0300

    Definição das variáveis e início da resolução do problema

--- condicionais/condicionais.js
+++ condicionais/condicionais.js
@@ -1,5 +1,5 @@
 console.log("\n\
-    Faça um programa para calcular o valor de uma viagem. Nele existirão cinco variáveis, sendo elas:\n\
+    Faça um programa para calcular o valor de uma viagem supondo o uso de um carro que faça 12 km/L quando usa gasolina e 9 km/L quando usa etanol. Nele existirão as seguintes variáveis obrigatórias:\n\
     \n\
         1 - preço do etanol;\n\
         2 - preço da gasolina;\n\
@@ -8,4 +8,21 @@ console.log("\n\
         5 - distância, em quilômetros, da viagem.\n\
     \n\
     Imprima no console o valor que será gasto para realizar a viagem.");
+
+let precoEtanol = 2.99; //Preço médio do etanol hoje em Campinas - SP
+let precoGasolina = 4.92; //Preço médio da gasolina hoje em Campinas - SP
+let combustivel;
+let gasolina;
+let etanol;
+let gastoCombustivel;
+let distanciaViagem = 648; //Distância, em quilômetros, de ida de Campinas (SP) à Cabo Frio (RJ)
+
+if(combustivel === gasolina){
+    gastoCombustivel = ((distanciaViagem * precoGasolina)/12);
+    console.log(gastoCombustivel);
+}else{
+    gastoCombustivel = ((distanciaViagem * precoEtanol)/9);
+    console.log(gastoCombustivel);
+};
+
 
