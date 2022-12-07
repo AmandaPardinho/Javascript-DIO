6cb87f845d55d2112c48c7549d003f05e81807bd
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-07 16:55:26 -0300

    Enunciado problema condicionais

---
+++ condicionais/condicionais.js
@@ -0,0 +1,11 @@
+console.log("\n\
+    Faça um programa para calcular o valor de uma viagem. Nele existirão cinco variáveis, sendo elas:\n\
+    \n\
+        1 - preço do etanol;\n\
+        2 - preço da gasolina;\n\
+        3 - tipo de combustível que está no carro;\n\
+        4 - gasto médio de combustível do carro, por quilômetro;\n\
+        5 - distância, em quilômetros, da viagem.\n\
+    \n\
+    Imprima no console o valor que será gasto para realizar a viagem.");
+
