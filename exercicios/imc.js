c4012a9b7d42c32b8be5d61f89dca897716cd955
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-08 16:55:18 -0300

    Enunciado do exercício proposto

---
+++ exercicios/imc.js
@@ -0,0 +1,13 @@
+console.log("O IMC (Índice de Massa Corpórea) é um critério da Organização Mundial de Saúde para dar uma indicação de peso de uma pessoa adulta.\n\
+\n\
+Fórmula do IMC:\n\
+IMC = peso / (altura * altura);\n\
+\n\
+Dada a fórmula do IMC, elabore um algoritmo que, dado o peso e a altura de uma pessoa adulta, mostre sua condição de acordo com a tabela abaixo:\n\
+\n\
+Condição do peso de adultos, de acordo com o IMC:\n\
+-Abaixo de 18.5: abaixo do peso;\n\
+-Entre 18.5 e 25: peso normalize;\n\
+-Entre 25 e 30: acima do peso;\n\
+-Entre 30 e 40: obeso;\n\
+-Acima de 40: obesidade mórbida.");
