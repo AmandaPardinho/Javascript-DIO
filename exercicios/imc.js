076e2781bdfe832262512df7067a71dff9421780
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-08 17:40:48 -0300

    Adição da função Math.pow e formatação do espaçamento das informações no terminal

--- exercicios/imc.js
+++ exercicios/imc.js
@@ -10,7 +10,7 @@ Condição do peso de adultos, de acordo com o IMC:\n\
 -Entre 18.5 e 25: peso normalize;\n\
 -Entre 25 e 30: acima do peso;\n\
 -Entre 30 e 40: obeso;\n\
--Acima de 40: obesidade mórbida.");
+-Acima de 40: obesidade mórbida.\n");
 
 const readline = require(`node:readline`);
 const {stdin: input, stdout: output} = require(`node:process`);
@@ -21,19 +21,19 @@ const leitor = readline.createInterface({
 });
 
 leitor.question("Qual o seu peso?", (answer1) =>{
-    leitor.question("Qual a sua altura?", (answer2) =>{
-        let imc = (+answer1 / (+answer2 * +answer2));
-        console.log(`Seu IMC é ${imc.toFixed(1)}`);
+    leitor.question("\nQual a sua altura?", (answer2) =>{
+        let imc = (+answer1 / Math.pow(+answer2, 2));
+        console.log(`\nSeu IMC é ${imc.toFixed(1)}`);
         if(imc < 18.5){
-            console.log("Abaixo do peso");
+            console.log("\nAbaixo do peso");
         }else if(imc >= 18.5 && imc < 25){
-            console.log("Peso normal");
+            console.log("\nPeso normal");
         }else if(imc >= 25 && imc < 30){
-            console.log("Acima do peso");
+            console.log("\nAcima do peso");
         }else if(imc >= 30 && imc < 40){
-            console.log("Obeso");
+            console.log("\nObeso");
         }else{
-            console.log("Obesidade mórbida");
+            console.log("\nObesidade mórbida");
         };
     });
 });
