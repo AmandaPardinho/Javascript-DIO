74f90fb75143c042a143617f5c9aeff84f95daf9
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-08 18:47:41 -0300

    Criada a parte lógica do código

--- exercicios/pagamento.js
+++ exercicios/pagamento.js
@@ -5,3 +5,25 @@ Condições de pagamento:\n\
 -À vista (dinheiro ou PIX): 15% de desconto;\n\
 -Em 2x: preço da etiqueta sem desconto;\n\
 -Acima de 2x: preço da etiqueta com acréscimo de 10% de juros.\n");
+
+//variáveis
+let debito;
+let dinheiroPix;
+let duasVezes;
+let acimaDuasVezes;
+let valorEtiqueta;
+let totalAPagar;
+
+if(debito === true){
+    totalAPagar = valorEtiqueta * 0.9;
+    console.log("O valor a ser pago é de R$ " + totalAPagar.toFixed(2));
+}else if(dinheiroPix === true){
+    totalAPagar = valorEtiqueta * 0.85;
+    console.log("O valor a ser pago é de R$ " + totalAPagar.toFixed(2));
+}else if(duasVezes === true){
+    totalAPagar = valorEtiqueta;
+    console.log("O valor a ser pago é de R$ " + totalAPagar.toFixed(2));
+}else {
+    totalAPagar = valorEtiqueta * 1.1;
+    console.log("O valor a ser pago é de R$ " + totalAPagar.toFixed(2));
+};
