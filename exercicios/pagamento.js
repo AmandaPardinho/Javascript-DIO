24a9214af2f5e4ec20201700da740bb79e336d0f
Amanda Pardinho <amanda.m.pardinho@gmail.com>
2022-12-08 17:55:47 -0300

    Criação do arquivo do novo exercício e seu enunciado

---
+++ exercicios/pagamento.js
@@ -0,0 +1,7 @@
+console.log("Elabore um algoritmo que calcule o que deve ser pago por um produto, coniderando o preço normal de etiqueta e a escolha da condição de pagamento. Utilize os códigos da tabela a seguir para ler qual a condição de pagamento escolhida e efetuar o cálculo adequado:\n\
+\n\
+Condições de pagamento:\n\
+-À vista (débito): 10% de desconto;\n\
+-À vista (dinheiro ou PIX): 15% de desconto;\n\
+-Em 2x: preço da etiqueta sem desconto;\n\
+-Acima de 2x: preço da etiqueta com acréscimo de 10% de juros.\n");
